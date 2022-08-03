c51ec90571b399dd29e1a72cd5cc9f883d10af84
leand <leandro.andrade.rocha@gmail.com>
2022-08-03 16:28:26 -0300

    Commit inicial

---
+++ src/main/java/me/dio/academia/digital/service/impl/AlunoServiceImpl.java
@@ -0,0 +1,70 @@
+package me.dio.academia.digital.service.impl;
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.entity.form.AlunoUpdateForm;
+import me.dio.academia.digital.infra.utils.JavaTimeUtils;
+import me.dio.academia.digital.repository.AlunoRepository;
+import me.dio.academia.digital.service.IAlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AlunoServiceImpl implements IAlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Override
+    public Aluno create(AlunoForm form) {
+        Aluno aluno = new Aluno();
+        aluno.setNome(form.getNome());
+        aluno.setCpf(form.getCpf());
+        aluno.setBairro(form.getBairro());
+        aluno.setDataDeNascimento(form.getDataDeNascimento());
+        return alunoRepository.save(aluno);
+    }
+
+    @Override
+    public Aluno get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Aluno> getAll() {
+        return alunoRepository.findAll();
+    }
+
+    @Override
+    public List<Aluno> getAll(String dataDeNascimento) {
+        if(dataDeNascimento == null) {
+            return alunoRepository.findAll();
+        }
+        LocalDate nascimento = LocalDate.parse(dataDeNascimento, JavaTimeUtils.LOCAL_DATE_FORMATTER);
+        return alunoRepository.findByDataDeNascimento(nascimento);
+    }
+
+    @Override
+    public Aluno update(Long id, AlunoUpdateForm formUpdate) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public List<AvaliacaoFisica> getAllAvaliacaoFisica(Long id) {
+        Optional<Aluno> aluno = alunoRepository.findById(id);
+        if(aluno.isPresent()) {
+            return aluno.get().getAvaliacoes();
+        }
+        return null;
+    }
+}
